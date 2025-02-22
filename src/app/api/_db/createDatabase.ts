61cdf4b4a2e0944636af2dce9995207df0bfd190
GentaAmeku <genta.ameku@gmail.com>
2025-02-22 11:35:46 +0900

    fix: db type

--- src/app/api/_db/createDatabase.ts
+++ src/app/api/_db/createDatabase.ts
@@ -4,6 +4,8 @@ export type Model<T> = {
   get: (cb: (data: T) => boolean) => T | undefined;
   insert: (value: T) => void;
   getAll: () => T[];
+  update(id: string, newData: Partial<T>): T | undefined;
+  remove(id: string): T | undefined;
 };
 
 export type Database<T> = () => Model<T>;
@@ -12,20 +14,20 @@ export const createDatabase = <T extends { id: string }>(
   initialData: T[],
 ): Database<T> => {
   const data = initialData;
-  return () => ({
+  return (): Model<T> => ({
     get: (cb) => data.find(cb),
     getAll: () => structuredClone(data),
     insert: (newData: T) => data.push({ ...newData, id: uuidv4() }),
     update: (id: string, newData: Partial<T>) => {
       const i = data.findIndex((d) => d.id === id);
-      if (i === -1) return null;
+      if (i === -1) return undefined;
       // Mutate
       data[i] = { ...data[i], ...newData };
       return data[i];
     },
     remove: (id: string) => {
       const index = data.findIndex((d) => d.id === id);
-      if (index === -1) return null;
+      if (index === -1) return undefined;
       // Mutate
       const removed = data.splice(index, 1)[0];
       return removed;
